bc06ad6b159baf373ba477863fe253b4a5a69b88
Daouda <daouda@gmail.com>
2026-04-15 20:10:29 +0200

    switch to pytorch (fix deploy)

--- app.py
+++ app.py
@@ -1,49 +1,35 @@
-from flask import Flask, render_template, request
-import tensorflow as tf
-import numpy as np
+import torch
 from PIL import Image
-
-app = Flask(__name__)
+import torchvision.transforms as transforms
+import gradio as gr
 
 classes = ["buildings", "forest", "glacier", "mountain", "sea", "street"]
 
-# Charger modèles
-models = {
-    "model1": tf.keras.models.load_model("model1.keras"),
-    "model2": tf.keras.models.load_model("model2.keras")
-}
+model = torch.load("daouda_model.pth", map_location="cpu")
+model.eval()
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    prediction = None
-    confidence = None
-    image_path = None
+transform = transforms.Compose([
+    transforms.Resize((150,150)),
+    transforms.ToTensor()
+])
 
-    if request.method == "POST":
-        file = request.files["image"]
-        model_name = request.form["model"]
-        model = models[model_name]
+def predict(image):
+    if image is None:
+        return "No image"
 
-        print("Model choisi:", model_name)
+    image = image.convert("RGB")
+    image = transform(image).unsqueeze(0)
 
-        image_path = "static/test.jpg"
-        file.save(image_path)
+    with torch.no_grad():
+        outputs = model(image)
+        _, predicted = torch.max(outputs, 1)
 
-        # 🔥 CORRECTION ICI
-        img = Image.open(image_path).convert("RGB").resize((150,150))
-        img = np.array(img) / 255.0
-        img = np.expand_dims(img, axis=0)
+    return classes[predicted.item()]
 
-        pred = model.predict(img)
-        class_index = np.argmax(pred)
-
-        prediction = classes[class_index]
-        confidence = round(np.max(pred) * 100, 2)
-
-    return render_template("index.html",
-                           prediction=prediction,
-                           confidence=confidence,
-                           image_path=image_path)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="pil"),
+    outputs="label",
+    title="Image Classifier",
+    description="Upload une image"
+).launch()
